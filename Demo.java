8fab20b6944f97d552b62592caa1cb161d2b44a5
sweety2909 <sweetysrivastava243@gmail.com>
2020-10-13 12:24:09 +0530

    Learning git

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo
+{
+  public static void main(String[] args)
+     
+     {
+          System.out.println("You have enterend in github");
+
+          System.out.println("\nYou have enterend in local repo");
+
+     }
+}
